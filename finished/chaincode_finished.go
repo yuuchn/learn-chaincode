bac8940fd35cf490159d432227a2f0b3894577ee
yuuchn <n.yuukiii@gmail.com>
2016-12-29 23:45:47 +0900

    カウントアップ

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -5,111 +5,106 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-		 http://www.apache.org/licenses/LICENSE-2.0
+         http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
 */
 
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"strconv"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
-// SimpleChaincode example simple Chaincode implementation
-type SimpleChaincode struct {
+// CounterChaincode example simple Chaincode implementation
+type CounterChaincode struct {
 }
 
+// カウンター情報
+type Counter struct {
+	Name string `json:"name"`
+	Counts int `json:"counts"`
+}
+
+const numOfCounters int = 3
+
+// カウンター情報の初期値を設定
+func (cc *CounterChaincode) Init(stub *shim.ChaincodeStub, function string, args []string)   ([]byte, error) {
+	var counters [numOfCounters]Counter
+	var countersBytes [numOfCounters][]byte
+
+	// カウンター情報を生成
+	counters[0] = Counter{Name: "Office Worker", Counts: 0}
+	counters[1] = Counter{Name: "Home Worker", Counts:0}
+	counters[2] = Counter{Name: "Student", Counts:0}
+
+	// カウンター情報をワールドステートメントに追加
+	for i := 0; i < len(counters); i++ {
+		// JSON形式に変換
+		countersBytes[i], _ = json.Marshal(counters[i])
+		// ワールドステートに追加
+		stub.PutState(strconv.Itoa(i), countersBytes[i])
+	}
+
+	return nil, nil
+}
+
+// カウンター情報を更新
+func (cc *CounterChaincode) Invoke(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+	// function名でハンドリング
+	if function == "countUp" {
+		// カウントアップを実行
+		return cc.countUp(stub, args)
+	}
+
+	return nil, errors.New("Received unknown function")
+}
+
+
+// カウンター情報を参照
+func (cc *CounterChaincode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+	// function名でハンドリング
+	if function == "refresh" {
+		// カウンター情報を取得
+		return cc.getCounters(stub, args)
+	}
+
+	return nil, errors.New("Received unknown function")
+}
+
+// カウントアップを実行
+func (cc *CounterChaincode) countUp(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+	// ワールドステートから選択されたカウンター情報を取得
+	counterId := args[0]
+	counterJson, _ := stub.GetState(counterId)
+
+	// 取得したJSON形式の情報をCounterに変換
+	counter := Counter{} // Counter構造体の初期化
+	json.Unmarshal(counterJson, &counter)
+
+	// カウントアップ
+	counter.Counts++
+
+	// ワールドステートに更新後の値を追加
+	counterJson, _ = json.Marshal(counter)
+	stub.PutState(counterId, counterJson)
+
+	return nil, nil
+}
+
+// Validationg Peerに接続し、チェーンコードを実行
 func main() {
-	err := shim.Start(new(SimpleChaincode))
+	err := shim.Start(new(CounterChaincode))
 	if err != nil {
-		fmt.Printf("Error starting Simple chaincode: %s", err)
+		fmt.Printf("Error starting chaincode: %s", err)
 	}
-}
-
-// Init resets all the things
-func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	if len(args) != 1 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 1")
-	}
-
-	err := stub.PutState("hello_world", []byte(args[0]))
-	if err != nil {
-		return nil, err
-	}
-
-	return nil, nil
-}
-
-// Invoke isur entry point to invoke a chaincode function
-func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	fmt.Println("invoke is running " + function)
-
-	// Handle different functions
-	if function == "init" {
-		return t.Init(stub, "init", args)
-	} else if function == "write" {
-		return t.write(stub, args)
-	}
-	fmt.Println("invoke did not find func: " + function)
-
-	return nil, errors.New("Received unknown function invocation: " + function)
-}
-
-// Query is our entry point for queries
-func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	fmt.Println("query is running " + function)
-
-	// Handle different functions
-	if function == "read" { //read a variable
-		return t.read(stub, args)
-	}
-	fmt.Println("query did not find func: " + function)
-
-	return nil, errors.New("Received unknown function query: " + function)
-}
-
-// write - invoke function to write key/value pair
-func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	var key, value string
-	var err error
-	fmt.Println("running write()")
-
-	if len(args) != 2 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the key and value to set")
-	}
-
-	key = args[0] //rename for funsies
-	value = args[1]
-	err = stub.PutState(key, []byte(value)) //write the variable into the chaincode state
-	if err != nil {
-		return nil, err
-	}
-	return nil, nil
-}
-
-// read - query function to read key/value pair
-func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	var key, jsonResp string
-	var err error
-
-	if len(args) != 1 {
-		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
-	}
-
-	key = args[0]
-	valAsbytes, err := stub.GetState(key)
-	if err != nil {
-		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
-		return nil, errors.New(jsonResp)
-	}
-
-	return valAsbytes, nil
 }
